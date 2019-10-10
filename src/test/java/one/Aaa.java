ebf618e53f1f0bef7bf62bc32a815fc1fff8679d
shujoncanada <johnshujon@gmail.com>
2019-10-10 12:55:43 -0400

    one

---
+++ src/test/java/one/Aaa.java
@@ -0,0 +1,20 @@
+package one;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Aaa {
+	
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\johns\\eclipse-workspace\\com.library.practise\\onlyDriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		
+		
+		System.out.println("japan");
+		
+	}
+
+}
